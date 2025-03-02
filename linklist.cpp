e84be0dab1bb7e5b97e70f815d3322f764144808
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-03-02 17:55:51 +0545

    Update linklist.cpp
    added function to display the elements in the list

--- linklist.cpp
+++ linklist.cpp
@@ -1,38 +1,40 @@
-# include<iostream>
+#include<iostream>
 using namespace std;
 
-struct Node{                                    // create head
-    int data;                                   // |--------|----------|
-    Node* next;                                 // |  data  | next     |   
-    Node(int value)                             // |--------|----------|
+struct Node{
+    int data;
+    Node* next;
+    Node(int value)
     {
-        data= value;
+        data=value;
         next=nullptr;
     }
 };
 
 class linklist{
     Node* head;
+    public:
     linklist()
     {
         head=nullptr;
     }
-    void insert_at_first(int data)
+    
+    void at_first(int data)
     {
-        new* newNode=new Node(data);    // make new Node
-        newNode->next=head;             // link with the next node hold the address of thas
-        head=newNode;                   
+        Node* newNode=new Node(data);
+        newNode->next=head;
+        head=newNode;
     }
     
     void append(int data)
     {
-        new* newNode=new Node(data);
+        Node* newNode=new Node(data);
         if(!head)
         {
-            newNode=head;
+            head=newNode;
             return;
         }
-        new* temp=head;
+        Node* temp=head;
         while(temp->next)
         {
             temp=temp->next;
@@ -40,5 +42,24 @@ class linklist{
         temp->next=newNode;
     }
     
-   
+    void display()
+    {
+        Node* temp=head;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<"----->";
+            temp=temp->next;
+        }
+        cout<<"Null"<<endl;
+    }
 };
+int main()
+{
+    linklist obj1;
+    obj1.at_first(1);
+    obj1.append(23);
+    obj1.append(45);
+    obj1.append(54);
+    obj1.display();
+    return 0;
+}
