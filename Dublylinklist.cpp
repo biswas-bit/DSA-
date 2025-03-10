7a65b62581127e587b5dc9bedf586d2d955c1e5e
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-03-10 22:31:47 +0545

    Create Dublylinklist.cpp

---
+++ Dublylinklist.cpp
@@ -0,0 +1,53 @@
+# include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* prev;
+    Node* next;
+    Node(int value)
+    {
+        data=value;
+        prev=nullptr;
+        next=nullptr;
+    }
+};
+
+class Dlinklist
+{
+    Node*head;
+    public:
+    Dlinklist()
+    {
+        head=nullptr;
+    }
+    
+    void at_first(int data)
+    {
+      Node* newNode=new Node(data);
+      newNode->next=head;
+      if(head!=nullptr)
+      {
+          head->prev=newNode;
+      }
+      head=newNode;
+    }
+    
+    void display()
+    {
+        Node* temp=head;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<"-->";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+int main()
+{
+    Dlinklist obj1;
+    obj1.at_first(1);
+    obj1.display();
+    return 0;
+}
