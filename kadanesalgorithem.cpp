fbea85388401105d627a47e780279aacab78e925
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-03-12 21:56:09 +0545

    Create kadanesalgorithem.cpp

---
+++ kadanesalgorithem.cpp
@@ -0,0 +1,25 @@
+# include<iostream>
+# include<vector>
+using namespace std;
+void  maxsumarray(vector<int> vec)
+{
+  int maxSum=0;
+  int currentSum=0;
+  for(int i=0; i<vec.size(); i++)
+    {
+      currentSum+=vec[i];
+      maxSum=max(currentSum, maxSum);
+      if(currentSum<0)
+      {
+        currentSum=0;
+      }
+    }
+  cout<<maxSum<<endl;
+  
+}
+int main()
+{
+   vector<int> arr={3,-4,5,4,-1,7,-8};
+    maxsumarray(arr);
+    return 0;
+}
