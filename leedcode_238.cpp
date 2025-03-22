cd1053735b4668aa1ba09fc01c5e70860e985f83
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-03-22 11:53:42 +0545

    Update leedcode_238.cpp optimized space and time comple

--- leedcode_238.cpp
+++ leedcode_238.cpp
@@ -49,6 +49,25 @@ vector<int>exceptSelf(vector<int>arr)   // O(n)
 }
 
 //optimizing space also
+vector<int>spaeOptimized(vector<int>& nums)
+{
+  int n=nums.size();
+  vector<int>ans(n,1);
+
+  //prefix
+  for(int i=0;i<n;i++)   //optimization
+    {
+      ans[i]=ans[i-1]+nums[i-1];
+    }
+
+  int sufix=1;
+for(int i-n-2;i>=0;i++)                         //optimization
+  {
+  sufix*=nums[i+1];
+  ans[i]*=sufix;
+  }
+  return ans;
+}
 
 
 int main()
