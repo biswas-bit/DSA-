18802f6d9c3226ffd8efbf4391208d71ebdca5af
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-03-23 21:00:24 +0545

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,35 @@
+// binary search
+# include<iostream>
+# include<vector>
+using namespace std;
+
+int binarySearch(vector<int>arr,int target)
+{
+   int start=0;
+   int end=arr.size()-1;
+   while(start<=end)
+   {
+       int mid=(start+end)/2;
+       if(arr[mid]>target)
+       {
+           end=mid-1;
+       }
+       else if(arr[mid]<target)
+       {
+           start=mid+1;
+       }
+       else{
+           return mid;
+       }
+   }
+   return -1;
+   
+}
+
+int main()
+{
+    vector<int>arr={1,2,3,4,5,6};
+    int index=binarySearch(arr,3);
+    cout<<index<<endl;
+    return 0;
+}
