1a87b9aff00f2d0c8fcde2105415638723513ca9
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-04-02 08:09:53 +0545

    Update bubbleshort.cpp optimized algo

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -3,15 +3,22 @@
 using namespace std;                       
 void bubbleSort(vector<int>arr)                 // time cop of this algorithem is O(n2) but we have more optimized malgorithem
 {
+ 
   for(int i=0;i<arr.size();i++)
     {
+       bool isSwaped=false;                                   // this algo still runs n-1 times even if the array is sorted to avoid we checked that the arr is swapping in the loop or not
       for(int j=0;j<arr.size()-i-1;j++)
         {
           if(arr[j]>arr[j+1])
           {
             swap(arr[j],arr[j+1]);
+            isSwaped=true;
           }
         }
+      if(!isSwaped)                  // if elements are not swapping the return 
+      {
+        return;
+      }
     }
 }
 int main()
