7bd5601ca0beaf7212f59bd3dab852752418a211
BISWAS.POKHREL <pokhrelbiswas53@gmail.com>
2025-04-10 21:34:20 +0545

    Update Dublylinklist.cpp

--- Dublylinklist.cpp
+++ Dublylinklist.cpp
@@ -1,69 +1,116 @@
-# include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node{
+class Node {
+public:
     int data;
-    Node* prev;
     Node* next;
-    Node(int value)
-    {
-        data=value;
-        prev=nullptr;
-        next=nullptr;
+    Node* prev;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+        prev = nullptr;
     }
 };
 
-class Dlinklist
-{
-    Node*head;
-    public:
-    Dlinklist()
-    {
-        head=nullptr;
-    }
-    
-    void at_first(int data)
-    {
-      Node* newNode=new Node(data);
-      newNode->next=head;
-      if(head!=nullptr)
-      {
-          head->prev=newNode;
-      }
-      head=newNode;
+class DoublyLinkedList {
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
     }
 
-void append(int data)
-{
-    Node* newNode=new Node(data);
-    if(!head)
-    {
-        head=newNode;
-        return;
+    void push_front(int data) {
+        Node* newNode = new Node(data);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
     }
-    Node*temp=head;
-    while(temp->next)
-        {
-            temp=temp->next;
+
+    void push_back(int data) {
+        Node* newNode = new Node(data);
+        if (!tail) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
         }
-    temp->next=newNode;
-    newNode->prev=temp;     //update prevoius Node
-}
-    void display()
-    {
-        Node* temp=head;
-        while(temp!=NULL)
-        {
-            cout<<temp->data<<"-->";
-            temp=temp->next;
+    }
+
+    void pop_front() {
+        if (!head) {
+            cout << "List is empty!" << endl;
+            return;
         }
-        cout<<"NULL"<<endl;
+        Node* temp = head;
+        if (head == tail) {  // eta Node
+            head = tail = nullptr;
+        } else {
+            head = head->next;
+            head->prev = nullptr;
+        }
+        delete temp;
+    }
+
+    void pop_back() {
+        if (!tail) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+        Node* temp = tail;
+        if (head == tail) {  // euta Node
+            head = tail = nullptr;
+        } else {
+            tail = tail->prev;
+            tail->next = nullptr;
+        }
+        delete temp;
+    }
+
+    void display_forward() {
+        Node* temp = head;
+        cout << "Forward: ";
+        while (temp) {
+            cout << temp->data << " <-> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    void display_backward() {
+        Node* temp = tail;
+        cout << "Backward: ";
+        while (temp) {
+            cout << temp->data << " <-> ";
+            temp = temp->prev;
+        }
+        cout << "NULL" << endl;
     }
 };
-int main()
-{
-    Dlinklist obj1;
-    obj1.at_first(1);
-    obj1.display();
+
+int main() {
+    DoublyLinkedList dll;
+
+    dll.push_front(10);
+    dll.push_front(20);
+    dll.push_back(5);
+    dll.display_forward();   // 20 <-> 10 <-> 5
+    dll.display_backward();  // 5 <-> 10 <-> 20
+
+    dll.pop_front();         // Removes 20
+    dll.pop_back();          // Removes 5
+
+    dll.display_forward();   // 10
+    dll.display_backward();  // 10
+
     return 0;
 }
